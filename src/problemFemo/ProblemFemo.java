078faaa9f0b3499b9642319c626cd6a861afbc19
Mange99 <you@example.com>
2021-12-04 20:57:29 +0100

    problemoFemo prep

--- src/problemFemo/ProblemFemo.java
+++ src/problemFemo/ProblemFemo.java
@@ -1,4 +1,26 @@
 package problemFemo;
 
-public class ProblemFemo {
+import Input.ReadFromFile;
+
+import java.io.File;
+import java.net.URL;
+import java.util.List;
+
+public class ProblemFemo extends ReadFromFile {
+
+    public ProblemFemo(){
+        URL url = getClass().getResource("input.txt");
+        File fileName = new File(url.getPath());
+        List<String> input = seperatedByLine(fileName);
+
+        System.out.println(problemUno(input));
+        System.out.println(problemDos(input));
+    }
+
+    private int problemUno(List<String> input){
+        return -1;
+    }
+    private int problemDos(List<String> input){
+        return -1;
+    }
 }
