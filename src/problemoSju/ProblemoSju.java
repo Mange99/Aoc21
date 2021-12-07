c7e7779638e52b4933312a640077669f10c7d4e8
Mange99 <you@example.com>
2021-12-07 07:31:15 +0100

    problemoSju cleanUp

--- src/problemoSju/ProblemoSju.java
+++ src/problemoSju/ProblemoSju.java
@@ -15,37 +15,14 @@ public class ProblemoSju extends ReadFromFile {
         File fileName = new File(url.getPath());
         List<Integer> input = seperatedByComma(fileName);
 
-        System.out.println(losSolvos(input));
-        System.out.println(Part2(input));
+        System.out.println(losSolvos(input, true));
+        System.out.println(losSolvos(input, false));
     }
-    private int sum(int x){
-        return x * (x+1)/2;
-    }
-
-    public int Part2(List<Integer> input) {
-        int sum = Integer.MAX_VALUE;
-
-        for (int i = 0; i <= 1000; i++) {
-            if(fuel2(input, i) < sum){
-                sum = fuel2(input, i);
-            }else{
-                return sum;
-            }
-        }
-        return sum;
-    }
-    private int fuel2(List<Integer> integers, int target){
-        int sum = 0;
-        for (int i = 0; i < integers.size(); i++) {
-            sum += sum(Math.abs(integers.get(i) - target));
-        }
-        return sum;
-    }
-    private int losSolvos(List<Integer> input){
+    private int losSolvos(List<Integer> input, boolean part1){
         int least = Integer.MAX_VALUE;
         for(int i = 0; i < 10000; i++){
-            if(fuel(input, i) < least){
-                least = fuel(input, i);
+            if(fuel(input, i, part1) < least){
+                least = fuel(input, i, part1);
             }else{
                 return least;
             }
@@ -53,11 +30,21 @@ public class ProblemoSju extends ReadFromFile {
         return least;
     }
 
-    private int fuel(List<Integer> integers, int target){
+    private int fuel(List<Integer> integers, int target, boolean a){
         int sum = 0;
         for (int i = 0; i < integers.size(); i++) {
-            sum += Math.abs(integers.get(i) - target);
+            if(a){
+                sum += Math.abs(integers.get(i) - target);
+
+            }else{
+                sum += sum(Math.abs(integers.get(i) - target));
+
+            }
         }
         return sum;
+    }
+
+    private int sum(int x){
+        return x * (x+1)/2;
     }
 }
