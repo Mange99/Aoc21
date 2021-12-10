1274235bb96e7d07de92484ebdbdf948b3d8152d
Mange99 <magnus.bryntheson@hotmail.com>
2021-12-10 13:29:05 +0100

    problemoTen

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,11 @@ import problemoAcht.ProblemoAcht;
 import problemoNeun.ProblemoNeun;
 import problemoSexis.ProblemoSexis;
 import problemoSju.ProblemoSju;
+import problemoTen.ProblemoTen;
 
 public class Main {
 
     public static void main(String[] args){
-        new ProblemoAcht();
+        new ProblemoTen();
     }
 }
