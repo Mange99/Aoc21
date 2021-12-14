17533aeadb51b10ebbd75d109461c322b886e9f7
Mange99 <magnus.bryntheson@hotmail.com>
2021-12-14 17:54:30 +0100

    fjorton del 12

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import problemFemo.ProblemFemo;
 import problemUno.ProblemUno;
 import problemoAcht.ProblemoAcht;
+import problemoFjorton.ProblemoFjorton;
 import problemoNeun.ProblemoNeun;
 import problemoSexis.ProblemoSexis;
 import problemoSju.ProblemoSju;
@@ -9,6 +10,6 @@ import problemoTen.ProblemoTen;
 public class Main {
 
     public static void main(String[] args){
-        new ProblemoTen();
+        new ProblemoFjorton();
     }
 }
---
+++ src/problemoFjorton/ProblemoFjorton.java
@@ -0,0 +1,64 @@
+package problemoFjorton;
+
+import Input.ReadFromFile;
+
+import java.io.File;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.Map.Entry.comparingByValue;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+
+public class ProblemoFjorton extends ReadFromFile {
+    String code = "";
+    List<String> input;
+
+    public ProblemoFjorton(){
+        URL url = getClass().getResource("input.txt");
+        File fileName = new File(url.getPath());
+        input = seperatedByLine(fileName);
+
+        code = input.get(0);
+        input.remove(0);
+        input.remove(0);
+
+        for(int i = 0; i < 10; i++){
+            problemoUno();
+        }
+        long max = code.chars()
+                .mapToObj(x -> (char) x)
+                .collect(groupingBy(x -> x, counting()))
+                .entrySet().stream()
+                .max(comparingByValue()).get().getValue();
+
+        long min = code.chars()
+                .mapToObj(x -> (char) x)
+                .collect(groupingBy(x -> x, counting()))
+                .entrySet().stream()
+                .min(comparingByValue()).get().getValue();
+
+        System.out.println(max-min);
+    }
+
+    private void problemoUno(){
+        for(int i = 0; i < code.length()-1; i += 2) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(code.charAt(i));
+            sb.append(code.charAt(i+1));
+
+            for(int j = 0; j < input.size(); j++){
+                String between = input.get(j).substring(0, 2);
+                String insert = input.get(j).substring(6);
+
+                if(between.equals(sb.toString())){
+                    code = code.substring(0, i+1) + insert + code.substring(i+1);
+                }
+            }
+        }
+    }
+}
